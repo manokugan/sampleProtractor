dd05bb478ab58b1254a1a252017e78c379098894
Manoujitha Kugamourthy <manoujitha@ip-10-1-22-231.ap-southeast-2.compute.internal>
2018-04-23 15:04:53 +1000

    Change to Subtract method

--- subtraction.js
+++ subtraction.js
@@ -1,17 +1,28 @@
 describe('protractor demo App', function () {
 	var spec = require('./spec.js');
 	const mySpec = new spec();
-	
-	var selectDropdownbyNum = function (element, optionNum) {   
-		var opt = element.all(by.tagName('option')).then(function (opt) {             
-			opt[optionNum].click();            
+
+	var selectDropdownbyNum = function (element) {
+		var opt = element.all(by.tagName('option'));
+		opt.each(function (eachOperator) {
+			eachOperator.getAttribute('value').then(function (value) {
+				if (value === 'SUBTRACTION') {
+					eachOperator.click();
+				}
+			});
 		});
 	};
+
+	/*var selectDropdownbyNum = function (element, optionNum) {   
+		var opt = element.all(by.tagName('option')).then(function (opt) {             
+			opt[optionNum].click();            
+		});
+	};*/
 
 	function subtract(a, b) {
 		mySpec.enterFirstNumber(a);
 		mySpec.enterSecondNumber(b);
-		selectDropdownbyNum(mySpec.getOperator(), 4);
+		selectDropdownbyNum(mySpec.getOperator());
 		mySpec.clickGoButton();
 	}
 
