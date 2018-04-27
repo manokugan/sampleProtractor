9ae45d438a33ad11da8bec6c1f707694e524a8a4
Manoujitha Kugamourthy <manoujitha@Datas-MacBook-Pro.local>
2018-04-27 16:11:30 +1000

    changes

--- addition.js
+++ addition.js
@@ -1,21 +1,22 @@
-describe('protractor demo App', function() {
+describe('protractor demo App', function () {
 
-	beforeEach(function() {
+	beforeEach(function () {
 		browser.get('http://juliemr.github.io/protractor-demo/');
 	});
-	 var spec = require('./spec.js');
+	var spec = require('./spec.js');
 	const mySpec = new spec();
+
 	function add(a, b) {
 		mySpec.enterFirstNumber(a);
 		mySpec.enterSecondNumber(b);
 		mySpec.clickGoButton();
 	}
-	
-	it('should have a title', function() {
+
+	it('should have a title', function () {
 		expect(browser.getTitle()).toEqual('Super Calculator');
 	});
 
-	it('should have a history', function() {
+	it('should have a history', function () {
 		add(1, 2);
 		mySpec.getLatestResult().then(console.log);
 		expect(mySpec.getLatestResult()).toEqual('3');
